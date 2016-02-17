0f03f11ba801b17ee6f795a69e4f475302111bb5
beatcracker <beatcracker@users.noreply.github.com>
2016-02-17 18:53:12 +0300

    Check target dir before deleting, verbose mode

--- unpack_jre.cmd
+++ unpack_jre.cmd
@@ -1,41 +1,51 @@
 @echo off
 setlocal
 
+set verbose=^>nul
 set pwd=%~dp0
 set zip=7z.exe
-set no_zip_msg=Can't find 7z.exe! Set full path to 7z.exe in %~nx0 if it's not in %%PATH%%!
 set jre_tmp=tmp
 set jre_src=JRE_Installer
 set jre_dst=JRE_Portable
 set jre_bin=installerexe
 set jre_packfiles=*.pack
+set jre_check=bin\java.exe
 
 where %zip% >nul 2>nul
 if %errorlevel%==1 (
-    echo %no_zip_msg%
+    echo Can't find 7z.exe! Set full path to 7z.exe in %~nx0 if it's not in %%PATH%%!
     goto :end
 )
 
 for /r "%pwd%%jre_src%\" %%a in ("jre-*.exe") do (
-	echo Processing JRE installer: %%a
+	echo Processing JRE installer: %%~nxa
 
-	echo Removing previous version: "%pwd%%jre_dst%\%%~na"
-	rd /s /q "%pwd%%jre_dst%\%%~na%"
+    if exist "%pwd%%jre_dst%\%%~na\%jre_check%" (
 
-	echo Unpacking "%%a" to "%pwd%%jre_dst%\%%~na\%jre_tmp%"
-	"%zip%" e "%%a" -aoa -o"%pwd%%jre_dst%\%%~na\%jre_tmp%"
+    	echo Removing previous version: "%pwd%%jre_dst%\%%~na"
+    	rd /s /q "%pwd%%jre_dst%\%%~na%"
+
+    )
+
+	echo Unpacking "%%~nxa" to "%pwd%%jre_dst%\%%~na\%jre_tmp%"
+	"%zip%" e "%%a" -aoa -o"%pwd%%jre_dst%\%%~na\%jre_tmp%" %verbose%
 
 	echo Unpacking "%pwd%%jre_dst%\%%~na\%jre_tmp%\%jre_bin%" to "%pwd%%jre_dst%\%%~na"
-	"%zip%" x "%pwd%%jre_dst%\%%~na\%jre_tmp%\%jre_bin%" -aoa -o"%pwd%%jre_dst%\%%~na"
+	"%zip%" x "%pwd%%jre_dst%\%%~na\%jre_tmp%\%jre_bin%" -aoa -o"%pwd%%jre_dst%\%%~na" %verbose%
 
-	echo Deleting: "%pwd%%jre_dst%\%%~na\%jre_tmp%"
-	rd /s /q "%pwd%%jre_dst%\%%~na\%jre_tmp%"
+    if exist "%pwd%%jre_dst%\%%~na\%jre_tmp%\%jre_bin%" (
+
+    	echo Deleting: "%pwd%%jre_dst%\%%~na\%jre_tmp%"
+    	rd /s /q "%pwd%%jre_dst%\%%~na\%jre_tmp%"
+
+    )
 
 	echo Unpacking %jre_packfiles% files
 	pushd "%pwd%%jre_dst%\%%~na"
 	for /r %%b in (%jre_packfiles%) do (
 
-		echo Unpacking "%%b" to "%%~db%%~pb%%~nb.jar"
+		rem echo "%%b" -^> "%%~db%%~pb%%~nb.jar" %verbose%
+        echo %%~nxb -^> %%~nb.jar %verbose%
 		"%pwd%%jre_dst%\%%~na\bin\unpack200" -r "%%b" "%%~db%%~pb%%~nb.jar"
 
 	)
@@ -45,15 +55,19 @@ for /r "%pwd%%jre_src%\" %%a in ("jre-*.exe") do (
 
 for /r "%pwd%%jre_src%\" %%a in ("jre-*.tar.gz") do (
 
-    echo Processing JRE archive: %%a
+    echo Processing JRE archive: %%~nxa
 
-    for /f "tokens=6 usebackq" %%J in (`call "%zip%" x "%%a" -so 2^>nul ^| "%zip%" l -si -ttar ^| findstr /r /i "jre[^^\\]*$"`) do (
+    for /f "tokens=6 usebackq" %%J in (`call "%zip%" x "%%a" -so 2^>nul ^| "%zip%" l -ttar -si ^| findstr /r /i "jre[^^\\]*$"`) do (
 
-    	echo Removing previous version: "%pwd%%jre_dst%\%%J"
-    	rd /s /q "%pwd%%jre_dst%\%%J"
+        if exist "%pwd%%jre_dst%\%%J\%jre_check%" (
     
+        	echo Removing previous version: "%pwd%%jre_dst%\%%J"
+        	rd /s /q "%pwd%%jre_dst%\%%J"
+    
+        )
+
     	echo Unpacking "%%J" to "%pwd%%jre_dst%\%%J"
-        "%zip%" x "%%a" -so 2>nul | "%zip%" x -aoa -ttar -o"%pwd%%jre_dst%" -si 1>nul
+        "%zip%" x "%%a" -so 2>nul | "%zip%" x -aoa -ttar -o"%pwd%%jre_dst%" -si %verbose%
 
     )
 
