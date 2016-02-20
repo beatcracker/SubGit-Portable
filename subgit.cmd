dbebe00a28bb4ab6129078b4e0506103d0543b77
beatcracker <beatcracker@users.noreply.github.com>
2016-02-20 21:38:48 +0300

    Fixed path to JRE dir

--- subgit.cmd
+++ subgit.cmd
@@ -4,7 +4,7 @@ setlocal
 rem Change jre_dir to match your unpacked JRE dir name in JRE_Portable
 set jre_dir=jre-8u40-windows-x64
 set subgit=%~dp0subgit\bin\subgit.bat
-set JAVA_HOME=%~dp0jre\%jre_dir%
+set JAVA_HOME=%~dp0JRE_Portable\%jre_dir%
 set HOME=%~dp0home
 
 %subgit% %*
