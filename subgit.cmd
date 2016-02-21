d1cf6e17353b646488497c476862e98755c4876f
beatcracker <beatcracker@users.noreply.github.com>
2016-02-21 19:57:00 +0300

    Check for valid JRE

--- subgit.cmd
+++ subgit.cmd
@@ -1,12 +1,23 @@
 @echo off
 setlocal
 
-rem Change jre_dir to match your unpacked JRE dir name in JRE_Portable
-set jre_dir=jre-8u40-windows-x64
-set subgit=%~dp0subgit\bin\subgit.bat
+rem Set jre_dir to match your unpacked JRE dir name in JRE_Portable
+set jre_dir=
+set jre_check=bin\java.exe
+set subgit=%~dp0SubGit\bin\subgit.bat
 set JAVA_HOME=%~dp0JRE_Portable\%jre_dir%
-set HOME=%~dp0home
 
-%subgit% %*
+if exist "%JAVA_HOME%\%jre_check%" (
+
+	%subgit% %*
+
+) else (
+
+	echo Can't find JRE in folder: "%jre_dir%"
+	echo Set name of the extracted JRE folder in "%~nx0".
+	echo Example: "set jre_dir=jre-8u40-windows-x64"
+	pause
+
+)
 
 endlocal
